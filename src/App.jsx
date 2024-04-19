b10bbacb0d3c8e66ebd2c4aa0ff2e86e620ebf7e
Freud Ovando Lara <freud5310@gmail.com>
2024-04-19 13:29:36 -0600

    i start with the fourtune cookies page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='App'>
+
+<h1 className="text-3xl font-bold underline ">
+      Hello world!
+    </h1>
+
+    <p>bol</p>
+
+   </div>
+  )
+}
+
+export default App
